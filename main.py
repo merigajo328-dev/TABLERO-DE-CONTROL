c7d6ffe8e58754c21ae21fa3a4af97489d0b2f39
merigajo@gmail.com <santiagoqw1234@gmail.com>
2025-09-08 09:46:01 -0500

    PRIMER ARCHIVO GRAFICOS

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hola mundo")
+import pandas as pd
+
+ruta= "https://github.com/VMjavier/claseAnalisisDatos/raw/refs/heads/main/datos_reales.zip"
+df=pd.rea_cvs(ruta)
+print(df.head())
+
+n =10 # Número de variables mas comunes a mostrar
+Columna = "Crime" # Cambia esto por la columna que deseas analizar
+
+topN = df[columna].value_counts().nlargest(n).index
+ptl.figure(figsize= (10,6))
+sns.countplot(data=df[df[columna].isin(topN)],
+           x=columna,
+            order=topN,
+            palette="tab10")
